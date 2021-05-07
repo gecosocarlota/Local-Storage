9bcfb517491de173cc37d5f13481959910f32533
gecosocarlota <carlotagecoso787@gmail.com>
2021-05-07 20:13:59 +0800

    First step with the project

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+// Variables
+
+
+//Event Listeners
+eventListeners();
+
+function eventListeners() {
+    //Form Submission
+    document.querySelector('#form').addEventListener('submit', newTweet);
+}
+
+// Function
+
+function newTweet(e) {
+    e.preventDefault();
+
+    console.log ('Form Submitted');
+}
